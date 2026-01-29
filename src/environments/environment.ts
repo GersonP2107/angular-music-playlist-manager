4fd885f4bdb30daded7270b49d41a32b104754fe
Gerson Pereira <140328852+GersonP2107@users.noreply.github.com>
2026-01-29 09:38:02 -0500

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    supabase: {
-        url: 'https://vbirmixfospgmjpnnqmi.supabase.co',
-        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaXJtaXhmb3NwZ21qcG5ucW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyMjEwNDksImV4cCI6MjA4NDc5NzA0OX0.hUodPD0Q2Rd97Ro_nfCR0xn88R6poft86Lh0ERp3Lds'
-    }
-};
